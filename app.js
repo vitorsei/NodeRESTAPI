40c1c065a99ce0c3c2c8e17c58e090bd83bd2329
vitorsei <seiji.vitor@gmail.com>
2016-11-03 17:31:24 -0200

    Getting a Single Item

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ bookRouter.route('/Books')
         });
     });
 
+bookRouter.route('/Books/:bookId')
+    .get(function (req, res) {
+
+        Book.findById(req.params.bookId, function (err, books) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        });
+    });
+
+
 app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
